f95899c635ba3eeefa8561de65c073983748d276
R4mBLe_ <misharad2008@gmail.com>
2024-06-03 19:44:41 +0300

    fix bug in thread

--- src/main/java/com/r4mble/dementia/commands/DementiaThread.java
+++ src/main/java/com/r4mble/dementia/commands/DementiaThread.java
@@ -19,7 +19,7 @@ public class DementiaThread extends Thread {
             }
             for (ServerPlayer player : DementiaMod.currentServer.getPlayerList().getPlayers()) {
                 player.setExperienceLevels(cooldownTime/1000); //DEBUG
-                int itemsRem = 3;
+                int itemsRem = Config.MAX_COUNT.get();
                 while (!player.getInventory().isEmpty() || itemsRem > 0) {
                     player.getInventory().items.get(random.nextInt(player.getInventory().items.size())).setCount(0);
                     itemsRem--;
