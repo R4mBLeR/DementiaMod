fa8fcac1a636fad82a0a5953faeb07e4024910dd
R4mBLe_ <misharad2008@gmail.com>
2024-06-21 23:00:25 +0300

    v1.0.4

--- src/main/java/com/r4mble/dementia/Config.java
+++ src/main/java/com/r4mble/dementia/Config.java
@@ -8,14 +8,16 @@ public class Config {
     public static final ForgeConfigSpec.ConfigValue<Integer> MIN_COOLDOWN;
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_COOLDOWN;
     public static final ForgeConfigSpec.ConfigValue<Integer> MAX_COUNT;
+    public static final ForgeConfigSpec.ConfigValue<Integer> TP_CHANCE;
 
 
     static {
         BUILDER.push("Dementia config");
 
-        MIN_COOLDOWN = BUILDER.comment("Minimun cooldown before things go missing ").define("MIN_COOLDOWN", 60);
-        MAX_COOLDOWN = BUILDER.comment("Maximum cooldown before things go missing").define("MAX_COOLDOWN", 300);
+        MIN_COOLDOWN = BUILDER.comment("Minimun cooldown before things go missing ").define("MIN_COOLDOWN", 0);
+        MAX_COOLDOWN = BUILDER.comment("Maximum cooldown before things go missing").define("MAX_COOLDOWN", 30);
         MAX_COUNT = BUILDER.comment("Maximum items count to missing").define("MAX_COUNT", 3);
+        TP_CHANCE = BUILDER.comment("Chance to teleport player in random point within one hundred blocks").define("TP_CHANCE", 90);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
