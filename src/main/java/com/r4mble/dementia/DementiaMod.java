911907081ded0ed963d5b4253ef4a253aae8eccf
R4mBLe_ <misharad2008@gmail.com>
2024-06-23 12:51:27 +0300

    v1.0.4a

--- src/main/java/com/r4mble/dementia/DementiaMod.java
+++ src/main/java/com/r4mble/dementia/DementiaMod.java
@@ -4,15 +4,11 @@ import com.r4mble.dementia.effects.ModEffects;
 import com.r4mble.dementia.items.ModItems;
 import com.r4mble.dementia.util.DementiaThread;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import java.util.Map;
-import java.util.Timer;
 
 
 @Mod(DementiaMod.MOD_ID)
@@ -20,7 +16,6 @@ public class DementiaMod {
     public static final String MOD_ID = "dementia";
     public static DementiaThread dementia = null;
     public static MinecraftServer currentServer = null;
-    public static Map<String, Timer> PLAYERS_TP_POS;
 
     public DementiaMod() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
