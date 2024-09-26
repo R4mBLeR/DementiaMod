906b81cc2e7a9b68fdf438febab6eed9b282d903
R4mBLe_ <misharad2008@gmail.com>
2024-09-26 21:05:57 +0300

    update forge version to 52.0.16

--- src/main/java/com/r4mble/dementia/DementiaMod.java
+++ src/main/java/com/r4mble/dementia/DementiaMod.java
@@ -5,21 +5,20 @@ import com.r4mble.dementia.items.ModItems;
 import com.r4mble.dementia.util.DementiaThread;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
 @Mod(DementiaMod.MOD_ID)
-public class DementiaMod {
+public class DementiaMod  {
     public static final String MOD_ID = "dementia";
     public static DementiaThread dementia = null;
     public static MinecraftServer currentServer = null;
 
-    public DementiaMod() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public DementiaMod(FMLJavaModLoadingContext context) {
+        context.registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
+        IEventBus eventBus = context.getModEventBus();
 
 
         ModEffects.EFFECTS.register(eventBus);
