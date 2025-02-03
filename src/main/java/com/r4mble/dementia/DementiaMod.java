d739d993e5b8c5cabd0ef3c3d1743ea44becfebc
R4mBLe_ <misharad2008@gmail.com>
2025-02-03 23:06:30 +0300

    v2.0

--- src/main/java/com/r4mble/dementia/DementiaMod.java
+++ src/main/java/com/r4mble/dementia/DementiaMod.java
@@ -4,15 +4,13 @@ import com.r4mble.dementia.effects.ModEffects;
 import com.r4mble.dementia.items.ModItems;
 import com.r4mble.dementia.util.DementiaThread;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import java.util.Map;
-import java.util.Timer;
 
 
 @Mod(DementiaMod.MOD_ID)
@@ -24,9 +22,16 @@ public class DementiaMod {
     public DementiaMod() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-
+        eventBus.addListener(this::addCreative);
+        
         ModEffects.EFFECTS.register(eventBus);
         ModItems.ITEMS.register(eventBus);
+    }
+
+    public void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.BLUE_ANTI_DEMENTIA_PILL);
+            event.accept(ModItems.RED_ANTI_DEMENTIA_PILL);
+        }
     }
 }
