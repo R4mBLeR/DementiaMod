9a79bf1484431bb31443920ba1590bf495a84415
R4mBLe_ <misharad2008@gmail.com>
2025-02-04 16:11:32 +0300

    v2.1

--- src/main/java/com/r4mble/dementia/DementiaMod.java
+++ src/main/java/com/r4mble/dementia/DementiaMod.java
@@ -2,26 +2,37 @@ package com.r4mble.dementia;
 
 import com.r4mble.dementia.effects.ModEffects;
 import com.r4mble.dementia.items.ModItems;
-import com.r4mble.dementia.util.DementiaThread;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import java.util.HashMap;
+
 
 @Mod(DementiaMod.MOD_ID)
-public class DementiaMod  {
+public class DementiaMod {
     public static final String MOD_ID = "dementia";
-    public static DementiaThread dementia = null;
-    public static MinecraftServer currentServer = null;
+    public static HashMap<ServerPlayer, Integer> dementiaPlayers;
 
     public DementiaMod(FMLJavaModLoadingContext context) {
         context.registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
         IEventBus eventBus = context.getModEventBus();
+        dementiaPlayers  = new HashMap<>();
+        eventBus.addListener(this::addCreative);
 
 
         ModEffects.EFFECTS.register(eventBus);
         ModItems.ITEMS.register(eventBus);
+    }
+
+    public void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.BLUE_ANTI_DEMENTIA_PILL);
+            event.accept(ModItems.RED_ANTI_DEMENTIA_PILL);
+        }
     }
 }
--- src/main/java/com/r4mble/dementia/items/ModItems.java
+++ src/main/java/com/r4mble/dementia/items/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
@@ -18,7 +19,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, DementiaMod.MOD_ID);
 
-    public static final RegistryObject<Item> ANTI_DEMENTIA_PILL = ITEMS.register("anti_dementia_pill",
+    public static final RegistryObject<Item> BLUE_ANTI_DEMENTIA_PILL = ITEMS.register("blue_anti_dementia_pill",
             () -> new Item(new Item.Properties().component(
                     DataComponents.CONSUMABLE,
                     Consumable.builder()
@@ -27,5 +28,16 @@ public class ModItems {
                                     new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(ModEffects.ANTI_DEMENTIA.getHolder().get(), 6000, 0), 1F)
                             )
                             .build()
-            ).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse( DementiaMod.MOD_ID+":anti_dementia_pill")))));
+            ).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse( DementiaMod.MOD_ID+":blue_anti_dementia_pill")))));
+
+    public static final RegistryObject<Item> RED_ANTI_DEMENTIA_PILL = ITEMS.register("red_anti_dementia_pill",
+            () -> new Item(new Item.Properties().component(
+                    DataComponents.CONSUMABLE,
+                    Consumable.builder()
+                            .hasConsumeParticles(false)
+                            .onConsume(
+                                    new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(ModEffects.ANTI_DEMENTIA.getHolder().get(), 12000, 0), 1F)
+                            )
+                            .build()
+            ).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse( DementiaMod.MOD_ID+":red_anti_dementia_pill")))));
 }
