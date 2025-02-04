7ff8597cba8181a630d4cf3bc44299a4f228c138
R4mBLe_ <misharad2008@gmail.com>
2025-02-04 15:42:33 +0300

    v2.1

--- src/main/java/com/r4mble/dementia/DementiaMod.java
+++ src/main/java/com/r4mble/dementia/DementiaMod.java
@@ -2,8 +2,7 @@ package com.r4mble.dementia;
 
 import com.r4mble.dementia.effects.ModEffects;
 import com.r4mble.dementia.items.ModItems;
-import com.r4mble.dementia.util.DementiaThread;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,16 +10,18 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import java.util.HashMap;
+
 
 @Mod(DementiaMod.MOD_ID)
 public class DementiaMod {
     public static final String MOD_ID = "dementia";
-    public static DementiaThread dementia = null;
-    public static MinecraftServer currentServer = null;
+    public static HashMap<ServerPlayer, Integer> dementiaPlayers;
 
     public DementiaMod(FMLJavaModLoadingContext context) {
         context.registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "dementia.toml");
         IEventBus eventBus = context.getModEventBus();
+        dementiaPlayers  = new HashMap<>();
         eventBus.addListener(this::addCreative);
 
 
